ab9a7cbbbd5d5b47c3f335ff0bd1832f728d81bf
TheCyberion <horelcyberion@gmail.com>
2022-10-20 18:34:04 +0200

    cette fois j'y crois

--- src/Controller/HostelController.php
+++ src/Controller/HostelController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Commentaire;
 use App\Repository\SliderRepository;
 use App\Repository\ChambreRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\CommentaireRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -39,4 +43,41 @@ class HostelController extends AbstractController
     {
         return $this->render('hostel/spas.html.twig');
     }
+
+    
+    #[Route('/avis/filtre', name:'avis_filtre')]
+    #[Route('/avis', name:'avis')]
+    public function avis(EntityManagerInterface $manager, Request $globals, CommentaireRepository $repo, $categorie = null)
+    {
+        
+        if($globals->request->get('category') != null){
+            $category= $globals->request->get('category');
+        }
+        $avisFiltre= $repo->findBy(["category" => $category]);
+        
+        $avis = $repo->findAll();
+
+        $comment = new Commentaire;
+        $form=$this->createForm(AvisType::class, $comment);
+        $form->handleRequest($globals);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $comment->setDateEnregistrement(new \DateTime);
+            $manager->persist($comment);
+            $manager->flush();
+            return $this->redirectToRoute("avis",[
+                'avis' => $avis,
+                'form' => $form,
+            ]);
+        }
+
+        return $this->renderForm('hostel/avis.html.twig',[
+            'avis' => $avis,
+            'form' => $form,
+            'category'=>$category,
+            'filtre'=> $avisFiltre
+        ]);
+    }
+    
 }
